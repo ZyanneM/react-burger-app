6bd58735f1d8de2e3c8f464ea5413f8e08f7297d
ZyanneM <92530551+ZyanneM@users.noreply.github.com>
2023-02-17 15:46:50 +0100

    modify function add structure

--- src/components/Panier.js
+++ src/components/Panier.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 const Panier = (props) => {
    const [count, setCount] = useState(0);
 
+   const [message, setMessage] = useState("Il n'y a aucun produit dans votre panier.");
+
     return (
         <div className="order-container">
         <h2>Votre Commande</h2>
